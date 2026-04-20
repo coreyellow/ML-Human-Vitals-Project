bb75e737fcf9fa4b2291eaf571d12da230da2b5a
coreyellow <lancemiguel@gmail.com>
2026-04-19 20:31:59 -0400

    Revert "Add scaled datasets"
    This reverts commit 62110423aa96761fffd179a0b65418a143663e2a.

--- dataset_prep.py
+++ dataset_prep.py
@@ -1,11 +1,8 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
 
 full_dataset = 'Datasets/full_dataset.csv'
 
 df = pd.read_csv(full_dataset)
-
-# Drop columns not needed for clustering
 df = df.drop(columns=[
     'Patient ID',
     'Timestamp',
@@ -18,8 +15,7 @@ df = df.drop(columns=[
 print("Reduced dataset shape: ", df.shape)
 
 df.to_csv('Datasets/reduced_dataset.csv', index=False)
-
-# Separate datasets by gender
+    
 df_male = df[df['Gender'] == 'Male']
 df_female = df[df['Gender'] == 'Female']
 
@@ -31,18 +27,3 @@ print("Female dataset shape: ", df_female.shape)
 
 df_male.to_csv('Datasets/male_dataset.csv', index=False)
 df_female.to_csv('Datasets/female_dataset.csv', index=False)
-
-# Standardize the datasets
-scaler = StandardScaler().set_output(transform="pandas")
-
-df_male = df_male.drop(columns=['Risk Category'])
-df_female = df_female.drop(columns=['Risk Category'])
-
-df_male_scaled = scaler.fit_transform(df_male)
-df_female_scaled = scaler.fit_transform(df_female)
-
-print("\nScaled male dataset shape:", df_male_scaled.shape)
-print("Scaled female dataset shape:", df_female_scaled.shape)
-
-df_male_scaled.to_csv('Datasets/male_scaled_dataset.csv', index=False)
-df_female_scaled.to_csv('Datasets/female_scaled_dataset.csv', index=False)
